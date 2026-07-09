fc817dd5dfcceceebbffbe3b6d8cf0926b6e9254
robertpitt <robertpitt1988@gmail.com>
2026-07-09 13:32:40 +0100

    Small cleanup

--- packages/sqlite/src/errors.ts
+++
@@ -1,5 +0,0 @@
-export * from "./SqliteMigrationError.ts";
-export * from "./SqliteOpenError.ts";
-export * from "./SqlitePathError.ts";
-export * from "./SqlitePragmaError.ts";
-export * from "./SqliteVectorUnavailableError.ts";
--- packages/sqlite/src/layer.ts
+++
@@ -1,7 +0,0 @@
-export {
-  makeSqliteLayer,
-  makeVectorSqliteLayer,
-  type SqliteLayerError,
-  type SqliteLayerOptions,
-  type SqliteVectorMode,
-} from "./SqliteLive.ts";
